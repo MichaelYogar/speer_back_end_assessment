fc6bda5b3483e6ed1a3b19ee7de261d82f9549b3
michaelyogar <myogar@uoguelph.ca>
2021-02-09 16:44:43 -0500

    switched to sequelize

--- server/db/index.js
+++ server/db/index.js
@@ -1,25 +1,19 @@
-const { Pool } = require("pg");
+const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const pool = new Pool({
-  user: process.env.PG_USER,
-  password: process.env.PG_PASS,
-  host: process.env.PG_HOST,
-  port: 5432 || process.env.PG_PORT,
-  database: process.env.PG_DB,
-  max: 20,
-  connectionTimeoutMillis: 0,
-  idleTimeoutMillis: 0,
-});
-
-pool.connect((err, client, release) => {
-  if (err) {
-    return console.error("Error acquiring client", err.stack);
-  } else {
-    return console.log("Connected to db");
+const sequelize = new Sequelize(
+  process.env.PG_DB,
+  process.env.PG_USER,
+  process.env.PG_PASS,
+  {
+    host: process.env.PG_HOST,
+    dialect: "postgres",
   }
-});
+);
 
-module.exports = {
-  query: (text, params) => pool.query(text, params),
-};
+sequelize
+  .authenticate()
+  .then(() => console.log("Connection established"))
+  .catch((err) => console.log("Error: " + err));
+
+module.exports = sequelize;
