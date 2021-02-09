56f91905d744fd1e2e644376756da61be17207bf
michaelyogar <myogar@uoguelph.ca>
2021-02-09 17:45:46 -0500

    successfully used orm for register function

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,46 +1,38 @@
-const express = require("express");
-const router = express.Router();
 const bcrypt = require("bcrypt");
 const db = require("../db");
-const { authorize, validate } = require("../middleware");
 const createJWT = require("../utils/createJWT");
+const User = require("../models/users");
 
 module.exports = {
   register: async (req, res) => {
     const { email, username, password } = req.body;
 
-    // create table if it doesn't exist because it's essential for user registration
     try {
-      const resultingTable = await db.query(
-        "CREATE TABLE IF NOT EXISTS users( \
-      user_id SERIAL,\
-      user_name VARCHAR(255) NOT NULL,\
-      user_email VARCHAR(255) NOT NULL UNIQUE,\
-      user_password VARCHAR(255) NOT NULL,\
-      PRIMARY KEY(user_id)\
-    )"
-      );
-      const user = await db.query("SELECT * FROM users WHERE user_email = $1", [
-        email,
-      ]);
+      const result = await User.findAll({
+        where: {
+          user_email: email,
+        },
+      });
 
       // user already exists, need to use different fields
-      if (user.rows.length > 0) {
+      if (result.length > 0) {
         return res.status(401).json("User already exist!");
       }
-
       const salt = await bcrypt.genSalt(10);
       const bcryptPassword = await bcrypt.hash(password, salt);
       // set initial balance for new users to 0
       const initialBalance = 0;
 
-      // returns the values of any columns after the insert or update was run
-      let newUser = await db.query(
-        "INSERT INTO users (user_name, user_email, user_password, balance) VALUES ($1, $2, $3, $4) RETURNING *",
-        [username, email, bcryptPassword, initialBalance]
-      );
+      const createdUser = await User.create({
+        user_name: username,
+        user_email: email,
+        user_password: bcryptPassword,
+        balance: initialBalance,
+      });
 
-      const jwtToken = createJWT(newUser.rows[0].user_id);
+      console.log("auto-generated ID:", createdUser.user_id);
+
+      const jwtToken = createJWT(createdUser.user_id);
 
       return res.json({ jwtToken });
     } catch (err) {
